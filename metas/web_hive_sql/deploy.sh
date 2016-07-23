ce2d3f5d13487612ae545200b7f8128c38e24a8c
AndyQu <717866228@qq.com>
2016-07-23 22:30:32 +0800

    Update deploy.sh

--- metas/web_hive_sql/deploy.sh
+++ metas/web_hive_sql/deploy.sh
@@ -1,5 +1,6 @@
 #@IgnoreInspection BashAddShebang
 #必须在srqserver工程根目录下面执行
+./gradlew tasks --all
 ./gradlew -i --stacktrace clean build 
 (java -server -jar build/libs/hivesql-0.1.0.jar > /tmp/logs/web-hive-sql/jar.log 2>&1 &)
 
