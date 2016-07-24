23a2efa5e82880dc12facda2c0105be7d5475713
andyqu <aichongqu@gmail.com>
2016-07-24 10:57:04 +0800

    try use bash -c to avoid strange syntax error

--- metas/web_hive_sql/deploy.sh
+++ metas/web_hive_sql/deploy.sh
@@ -1,4 +1,4 @@
 #@IgnoreInspection BashAddShebang
 #必须在srqserver工程根目录下面执行
 /bin/bash -c "./gradlew --info clean build" 
-(java -server -jar build/libs/hivesql-0.1.0.jar > /tmp/logs/web-hive-sql/jar.log 2>&1 &)
+/bin/bash -c "(java -server -jar build/libs/hivesql-0.1.0.jar > /tmp/logs/web-hive-sql/jar.log 2>&1 &)"
