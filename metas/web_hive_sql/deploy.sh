fb9092fd554fdc84fb3d3851274577d0940d3037
andyqu <aichongqu@gmail.com>
2016-07-24 17:55:31 +0800

    add  -Dfile.encoding=UTF-8

--- metas/web_hive_sql/deploy.sh
+++ metas/web_hive_sql/deploy.sh
@@ -1,4 +1,6 @@
 #@IgnoreInspection BashAddShebang
 #必须在srqserver工程根目录下面执行
 /bin/bash -c "./gradlew --info clean build" 
-/bin/bash -c "(java -server -jar build/libs/hivesql-0.1.0.jar > /tmp/logs/web-hive-sql/jar.log 2>&1 &)"
+
+#syntax error near unexpected token `$'\r''
+/bin/bash -c "(java -Dfile.encoding=UTF-8 -server -jar build/libs/hivesql-0.1.0.jar > /tmp/logs/web-hive-sql/jar.log 2>&1 &)"
