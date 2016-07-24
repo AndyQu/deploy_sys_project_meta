5c4881085bb2e1e1bbecf92787199015fcc6e07f
andyqu <aichongqu@gmail.com>
2016-07-24 10:51:07 +0800

    must use bash -c under windows. otherwise task name will not be correctly passed

--- metas/web_hive_sql/deploy.sh
+++ metas/web_hive_sql/deploy.sh
@@ -1,6 +1,4 @@
 #@IgnoreInspection BashAddShebang
 #必须在srqserver工程根目录下面执行
-./gradlew --debug --stacktrace tasks
-./gradlew --debug --stacktrace clean build 
+/bin/bash -c "./gradlew --info clean build" 
 (java -server -jar build/libs/hivesql-0.1.0.jar > /tmp/logs/web-hive-sql/jar.log 2>&1 &)
-
